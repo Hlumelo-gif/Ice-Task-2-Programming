f9d6def359a095fe4f19582227573f35e6012606
Hlumelo-gif <hlumelomasinyane3@gmail.com>
2026-09-11 17:53:42 +0200

    Add number divisibility check for 2 or 3

---
+++ question4.py
@@ -0,0 +1,9 @@
+# Question 4
+# get a number from the user
+num = int(input("Enter a number: "))
+
+# check if divisible by 2 OR 3
+if num % 2 == 0 or num % 3 == 0:
+print("The number is divisible by 2 or 3.")
+else:
+print("The number is not divisible by 2 or 3.")
